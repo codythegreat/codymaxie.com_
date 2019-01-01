3f1d239b57371268841700bdb19fac39b9bd8794
codythegreat <maxiecody@gmail.com>
2019-01-01 21:57:37 +0000

    added to skills, linked resume

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const textAnimElem = document.getElementById('text-animation');
 
 let possibleText = [
-  'webDeveloper','frontEndDesigner',
-  'backEndBuilder','hobbyServerAdmin',
+  'fullStackDeveloper', 'frontEndDesigner',
+  'backEndBuilder', 'hobbyServerAdmin',
   'linuxEnthusiast', 'pythonProgrammer',
   'bashPowerUser', 'taskAutomator'
 ];
 let currentText = 0;
 
 const textAnimation = () => {
-  
-  let intervalTypeText = setInterval(function() {
+
+  let intervalTypeText = setInterval(function () {
     typeText(possibleText[currentText])
   }, 200);
-  
+
   const typeText = (text) => {
     if (document.getElementById('text-animation').textContent == text) {
       if (currentText == possibleText.length - 1) {
@@ -22,29 +22,40 @@ const textAnimation = () => {
         currentText++;
       }
       clearInterval(intervalTypeText);
-      setTimeout(function() {
+      setTimeout(function () {
         document.getElementById('text-animation').textContent = "";
-        textAnimation()}, 1000);
+        textAnimation()
+      }, 1000);
     } else {
       document.getElementById('text-animation').textContent = text.substring(0, document.getElementById('text-animation').textContent.length + 1);
     }
   }
 }
 
-document.getElementById('container-skills').onmouseover = function(){timeoutFadeInSkills()};
+document.getElementById('container-skills').onmouseover = function () {
+  timeoutFadeInSkills()
+};
 
 const timeoutFadeInSkills = () => {
   fadeInSkills(0);
-  setTimeout(function() {fadeInSkills(1);}, 700);
-  setTimeout(function() {fadeInSkills(2);}, 1400);
-  setTimeout(function() {fadeInSkills(3);}, 2100);
-  setTimeout(function() {fadeInSkills(4);}, 2800);
+  setTimeout(function () {
+    fadeInSkills(1);
+  }, 700);
+  setTimeout(function () {
+    fadeInSkills(2);
+  }, 1400);
+  setTimeout(function () {
+    fadeInSkills(3);
+  }, 2100);
+  setTimeout(function () {
+    fadeInSkills(4);
+  }, 2800);
   document.getElementById('container-skills').onmouseover = "";
 };
 
 const fadeInSkills = (list) => {
   currentOpac = 0.0;
-  let intervalIncreaseOpac = setInterval(function() {
+  let intervalIncreaseOpac = setInterval(function () {
     addOpac();
   }, 10);
   const addOpac = () => {
@@ -58,6 +69,3 @@ const fadeInSkills = (list) => {
     }
   }
 };
-
-
-  
